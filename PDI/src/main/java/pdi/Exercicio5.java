5c31fadd7ccd1f30644a8de02693f3651d4b5e3c
GuiCT <gtomiasi@gmail.com>
2023-04-11 19:22:14 -0300

    exercicio da Aula 05 de PDI

---
+++ PDI/src/main/java/pdi/Exercicio5.java
@@ -0,0 +1,37 @@
+package pdi;
+
+import matrix.MatrixPGM;
+import matrix.OperationI;
+
+public class Exercicio5 {
+    public static void main(String[] args) {
+        try {
+            MatrixPGM lena = MatrixPGM.readFile("resources/lena256.pgm");
+            MatrixPGM lenaPrimeiraFuncao = lena.copy();
+            OperationI primeiraFuncao = (value, maxVal) -> {
+                if (value > 100 && value < 200) {
+                    return 150;
+                }
+                
+                return 0;
+            };
+            lenaPrimeiraFuncao = lenaPrimeiraFuncao
+                    .applyElementWiseOperation(primeiraFuncao);
+            MatrixPGM lenaSegundaFuncao = lena.copy();
+            OperationI segundaFuncao = (value, maxVal) -> {
+                if (value > 100 && value < 200) {
+                    return 150;
+                }
+                
+                return value;
+            };
+            lenaSegundaFuncao = lenaSegundaFuncao
+                    .applyElementWiseOperation(segundaFuncao);
+            lenaPrimeiraFuncao.saveToFile("resources/lenaex5a.pgm");
+            lenaSegundaFuncao.saveToFile("resources/lenaex5b.pgm");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+    }
+}
