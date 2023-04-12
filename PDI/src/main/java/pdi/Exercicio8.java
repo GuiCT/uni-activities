11b03edc6705acb838c7c8835cfe617a9a9d46e3
GuiCT <gtomiasi@gmail.com>
2023-04-12 16:49:17 -0300

    exercício de formação de vetor representando histograma da imagem

---
+++ PDI/src/main/java/pdi/Exercicio8.java
@@ -0,0 +1,29 @@
+package pdi;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import matrix.MatrixPGM;
+
+// Aula 05 - Construção de Histograma
+public class Exercicio8 {
+    public static void main(String[] args) {
+        try {
+            MatrixPGM lena = MatrixPGM.readFile("resources/lena256.pgm");
+            int[] histogramVector = new int[lena.maxVal];
+            for (int i = 0; i < lena.lines; i++) {
+                for (int j = 0; j < lena.columns; j++) {
+                    histogramVector[lena.values[i][j]]++;
+                }
+            }
+            FileWriter histogramFile = new FileWriter("resources/lenaHistogram.csv");
+            BufferedWriter bfw = new BufferedWriter(histogramFile);
+            for (int i = 0 ; i < lena.maxVal; i++)
+                bfw.write(String.format("%d,", histogramVector[i]));
+            bfw.close();
+            histogramFile.close();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
